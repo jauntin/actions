6a2338d298b0ec5ae4b8f04d19ca93752f1fac4c
Sid Momin <sid.momin@mmkkyweb.com>
2023-11-21 18:24:24 -0500

    Updates project cleanup action

--- project-maintenance/index.js
+++ project-maintenance/index.js
@@ -1,40 +1,22 @@
 import core from "@actions/core";
 import { Octokit } from "@octokit/core";
-import {
-  getProjectDetails,
-  getProjectItems,
-  toSetToCurrentIterationFilter,
-  toArchiveFilter,
-  setItemToCurrentIteration,
-  archiveItem,
-} from "./functions.js";
+import { Project } from "./Project.js";
 
 const run = async () => {
   try {
-    const owner = core.getInput("owner");
-    const number = Number(core.getInput("number"));
-    const token = core.getInput("token");
-
-    const octokit = new Octokit({ auth: token });
-
-    const projectDetails = await getProjectDetails({ octokit, owner, number });
-    const items = await getProjectItems({ octokit, owner, number });
-
-    await Promise.all(
-      toSetToCurrentIterationFilter(projectDetails, items).map((item) =>
-        setItemToCurrentIteration({ octokit, owner, projectDetails, item })
-      )
-    );
-    await Promise.all(
-      toArchiveFilter(projectDetails, items).map((item) =>
-        archiveItem({
-          octokit,
-          owner,
-          projectId: projectDetails.id,
-          itemId: item.id,
-        })
-      )
-    );
+    const project = new Project({
+      owner: core.getInput("owner"),
+      number: Number(core.getInput("number")),
+      owner: core.getInput("owner"),
+      iterationFieldName: core.getInput("iterationFieldName"),
+      statusFieldName: core.getInput("statusFieldName"),
+      doneStatusName: core.getInput("doneStatusName"),
+      daysToKeepOnDone: Number(core.getInput("daysToKeepOnDone")),
+      octokit: new Octokit({ auth: core.getInput("token") })
+    });
+    await project.retrieveProjectData();
+    await project.setItemsToCurrentIteration();
+    await project.archiveItems();
   } catch (error) {
     core.setFailed(error);
   }
